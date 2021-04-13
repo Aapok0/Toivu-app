2061b74ea9ec61d6fc5a3b16c938fefffe2992ca
roniheininen <roni.heininen@metropolia.fi>
2021-04-13 09:07:21 +0300

    Made some visual changes to graphs

---
+++ Toivu/js/ToivuTheme.js
@@ -0,0 +1,8 @@
+function ToivuTheme(target) {
+    if (target instanceof am4core.ColorSet) {
+      target.list = [
+        am4core.color("#BB8588"),
+        am4core.color("#D8A48F")
+      ];
+    }
+  }
--- Toivu/userAccount.php
+++ Toivu/userAccount.php
@@ -49,6 +49,7 @@
                 <script src="https://cdn.amcharts.com/lib/4/core.js"></script>
                 <script src="https://cdn.amcharts.com/lib/4/charts.js"></script>
                 <script src="https://cdn.amcharts.com/lib/4/themes/animated.js"></script>
+                <script src="scripts/ToivuTheme.js"></script>
                 
                 <script src="scripts/graph1.js"></script>
             </div>
