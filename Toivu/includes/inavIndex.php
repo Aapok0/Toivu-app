7c8d4accdd8785ca5771b8fa17bb0e49674410a9
Aapok0 <aapo@kokot.fi>
2021-04-15 18:42:21 +0300

    Added new logo and nav-icons, fixed pNN50

--- Toivu/includes/inavIndex.php
+++ Toivu/includes/inavIndex.php
@@ -6,12 +6,12 @@
         $pos = strrpos($url, "userAccount.php"); 
 
         if ($pos != false) {
-            echo("<li class=\"nav__item active\"><a href=\"userAccount.php\">" . $_SESSION['suserName'] . ":n sivu</a></li><br />");
-            echo("<li class=\"nav__item\"><a href=\"logOutUser.php\">Uloskirjautuminen</a></li><br />");
+            echo("<li class=\"nav__item active\"><a href=\"userAccount.php\"><img class=\"nav-icon\" src=\"images/iconfinder_heartbeat_1608928.png\" alt=\"Profile\"><span>" . $_SESSION['suserName'] . ":n sivu</span></a></li><br />");
+            echo("<li class=\"nav__item\"><a href=\"logOutUser.php\"><img class=\"nav-icon\" src=\"images/iconfinder_sign-out_298869.png\" alt=\"Profile\"><span>Uloskirjautuminen</span></a></li><br />");
         }
         else {
-            echo("<li class=\"nav__item\"><a href=\"userAccount.php\">" . $_SESSION['suserName'] . ":n sivu</a></li><br />");
-            echo("<li class=\"nav__item\"><a href=\"logOutUser.php\">Uloskirjautuminen</a></li><br />");
+            echo("<li class=\"nav__item\"><a href=\"userAccount.php\"><img class=\"nav-icon\" src=\"images/iconfinder_heartbeat_1608928.png\" alt=\"Profile\"><span>" . $_SESSION['suserName'] . ":n sivu</span></a></li><br />");
+            echo("<li class=\"nav__item\"><a href=\"logOutUser.php\"><img class=\"nav-icon\" src=\"images/iconfinder_sign-out_298869.png\" alt=\"Profile\"><span>Uloskirjautuminen</span></a></li><br />");
         }
     }
     else {
@@ -20,16 +20,16 @@
         $pos2 = strrpos($url, "createAccount.php");
 
         if ($pos1 != false) {
-            echo("<li class=\"nav__item active\"><a href=\"logInUser.php\">Kirjaudu sisään</a></li>");
-            echo("<li class=\"nav__item\"><a href=\"createAccount.php\">Rekisteröinti</a></li>");
+            echo("<li class=\"nav__item active\"><a href=\"logInUser.php\"><img class=\"nav-icon\" src=\"images/iconfinder_sign-in_298868.png\" alt=\"Profile\"><span>Kirjaudu sisään</span></a></li>");
+            echo("<li class=\"nav__item\"><a href=\"createAccount.php\"><img class=\"nav-icon\" src=\"images/iconfinder_add_user_309049.png\" alt=\"Profile\"><span>Rekisteröinti</a></span></li>");
         }
         else if ($pos2 != false) {
-            echo("<li class=\"nav__item\"><a href=\"logInUser.php\">Kirjaudu sisään</a></li>");
-            echo("<li class=\"nav__item active\"><a href=\"createAccount.php\">Rekisteröinti</a></li>");
+            echo("<li class=\"nav__item\"><a href=\"logInUser.php\"><img class=\"nav-icon\" src=\"images/iconfinder_sign-in_298868.png\" alt=\"Profile\"><span>Kirjaudu sisään</span></a></li>");
+            echo("<li class=\"nav__item active\"><a href=\"createAccount.php\"><img class=\"nav-icon\" src=\"images/iconfinder_add_user_309049.png\" alt=\"Profile\"><span>Rekisteröinti</a></span></li>");
         }
         else {
-            echo("<li class=\"nav__item\"><a href=\"logInUser.php\">Kirjaudu sisään</a></li>");
-            echo("<li class=\"nav__item\"><a href=\"createAccount.php\">Rekisteröinti</a></li>");
+            echo("<li class=\"nav__item\"><a href=\"logInUser.php\"><img class=\"nav-icon\" src=\"images/iconfinder_sign-in_298868.png\" alt=\"Profile\"><span>Kirjaudu sisään</span></a></li>");
+            echo("<li class=\"nav__item\"><a href=\"createAccount.php\"><img class=\"nav-icon\" src=\"images/iconfinder_add_user_309049.png\" alt=\"Profile\"><span>Rekisteröinti</a></span></li>");
         }
     }
 ?>
